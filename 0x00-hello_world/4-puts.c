9096e6951df9573ee1c6cfb75dc8f822b08315c5
Muhammad Abubakar Muhammad <muhammadabubakar737@gmail.com>
2023-03-16 13:10:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,16 @@
 
 
 /**
- * main - prints " a line of code using puts
+ * main - prints "Programming is like building a multilingual puzzle
  * puzzle, followed by a newline."
  * return 0.
 */
 
 int main(void)
+
 {
-        puts("\"programming is like building a multilingual puzzle");
+
+        puts("\"Programming is like building a multilingual puzzle");
  
 	return (0);
 }
