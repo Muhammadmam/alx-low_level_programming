e5089cb0b9d39c03306ef5078b0f1120a4db3943
Muhammad Abubakar Muhammad <muhammadabubakar737@gmail.com>
2023-03-17 17:56:59 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Descrition: 'print alphabet lowercase and uppercase'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for(n = 0; n <= 0; n++;)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n')
+	return (0);
+}
