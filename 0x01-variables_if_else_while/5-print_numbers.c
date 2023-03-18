07aa79175720abd4b2fdd8279f802e4fc6e42438
Muhammad Abubakar Muhammad <muhammadabubakar737@gmail.com>
2023-03-17 17:06:00 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Descrition: 'print alphabet lowercase and uppercase'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for ( = 0; n <= 0; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
