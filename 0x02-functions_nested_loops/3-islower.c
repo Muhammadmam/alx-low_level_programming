ba1ca3bc1caea2a0518f0868d8c3d2ca0012c391
Muhammad Abubakar Muhammad <muhammadabubakar737@gmail.com>
2023-03-21 10:08:20 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,8 @@
 
 /**
  * _islower - checks for lowercase character
- * @c: the character to be checked
- * Retun: 1 for lowercase character or 0 for anything else
+ * @c: The character to be checked
+ * Return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int c)
